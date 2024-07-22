70fb70424d9f95515008088bb583e9db4dbc6b6f
a.chugunov <a.chugunov@kefirgames.com>
2024-07-22 14:20:52 +0300

    ssssss

--- index.js
+++ index.js
@@ -9,45 +9,21 @@ const bot = new TelegramBot(TOKEN, {
 const port = process.env.PORT || 5000;
 const gameName = "CastleSurvivor";
 const queries = {};
-
 server.use(express.static(path.join(__dirname, 'CastleSurvivorV2')));
-
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
-
-bot.onText(/start|game/, (msg) => {
-    const options = {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Play Castle Survival', callback_data: 'game' }],
-                [{ text: 'Invite Friends', callback_data: 'invite' }, { text: 'Top Up Balance', callback_data: 'balance' }],
-                [{ text: 'Shop', callback_data: 'shop' }, { text: 'News', callback_data: 'news' }]
-            ]
-        }
-    };
-    bot.sendMessage(msg.from.id, "Welcome to the game!", options);
-});
-
+bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
-    if (query.data === 'game') {
+    if (query.game_short_name !== gameName) {
+        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+    } else {
+        queries[query.id] = query;
         let gameurl = "https://likepenza1.github.io/CastleSurvivor/";
-        bot.answerCallbackQuery(query.id, {
+        bot.answerCallbackQuery({
+            callback_query_id: query.id,
             url: gameurl
         });
-    } else if (query.data === 'invite') {
-        // Add your code for invite friends here
-        bot.answerCallbackQuery(query.id, "Invite friends functionality is not implemented yet.");
-    } else if (query.data === 'balance') {
-        // Add your code for top up balance here
-        bot.answerCallbackQuery(query.id, "Top up balance functionality is not implemented yet.");
-    } else if (query.data === 'shop') {
-        // Add your code for shop here
-        bot.answerCallbackQuery(query.id, "Shop functionality is not implemented yet.");
-    } else if (query.data === 'news') {
-        // Add your code for news here
-        bot.answerCallbackQuery(query.id, "News functionality is not implemented yet.");
     }
 });
-
 bot.on("inline_query", function (iq) {
     bot.answerInlineQuery(iq.id, [{
         type: "game",
@@ -55,7 +31,6 @@ bot.on("inline_query", function (iq) {
         game_short_name: gameName
     }]);
 });
-
 server.get("/highscore/:score", function (req, res, next) {
     if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
     let query = queries[req.query.id];
@@ -73,5 +48,4 @@ server.get("/highscore/:score", function (req, res, next) {
     bot.setGameScore(query.from.id, parseInt(req.params.score), options,
         function (err, result) {});
 });
-
 server.listen(port);
