1291828714e0b381a8b33994405a805ac76399e5
a.chugunov <a.chugunov@kefirgames.com>
2024-08-12 17:58:56 +0300

    chg: telegram bot API token

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,15 +3,10 @@ import telegram
 
 async def main():
     bot = telegram.Bot(token='7383514055:AAG6zoxC3GjdM0g6DVdJiACucVaTzYcQp4c')
-    updates = await bot.get_updates()
+    update = await bot.get_updates()[-1]
+    username = update.message.from_user.username
 
-    if updates:
-        update = updates[-1]
-        username = update.message.from_user.username
-
-        with open('username.txt', 'w') as f:
-            f.write(username)
-    else:
-        print("No updates available.")
+    with open('username.txt', 'w') as f:
+        f.write(username)
 
 asyncio.run(main())
