5b21611ff1a7e9304d55e46e0ea6eb884874acf3
a.chugunov <a.chugunov@kefirgames.com>
2024-08-12 18:29:36 +0300

    chg: tg bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,22 @@
 import asyncio
 import telegram
+import psutil
+import sys
+
+
+# Get the name of the current script
+script_name = sys.argv[0]
+
+# Get a list of all running processes
+processes = psutil.process_iter(['pid', 'name'])
+
+# Check if there are any other instances of the script running
+for process in processes:
+    if process.info['name'] == script_name and process.info['pid'] != os.getpid():
+        print(f"Another instance of the script is already running with PID {process.info['pid']}. Exiting...")
+        sys.exit()
+
+# Your code to get updates from Telegram bot goes here
 
 async def main():
     bot = telegram.Bot(token='7383514055:AAG6Kg93Lra_Unq3TmIYkzuAlPhp75J9vP0')
