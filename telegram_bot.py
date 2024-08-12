ec618d7e4470b0994917c6097b2b2965a9c9e8f4
a.chugunov <a.chugunov@kefirgames.com>
2024-08-12 18:01:57 +0300

    chg: tg bot script

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,10 +3,15 @@ import telegram
 
 async def main():
     bot = telegram.Bot(token='7383514055:AAG6zoxC3GjdM0g6DVdJiACucVaTzYcQp4c')
-    update = await bot.get_updates()[-1]
-    username = update.message.from_user.username
+    updates = await bot.get_updates()
 
-    with open('username.txt', 'w') as f:
-        f.write(username)
+    if updates:
+        update = updates[-1]
+        username = update.message.from_user.username
+
+        with open('username.txt', 'w') as f:
+            f.write(username)
+    else:
+        print("No updates available.")
 
 asyncio.run(main())
