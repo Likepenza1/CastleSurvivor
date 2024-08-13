8dcb4844f81798c8cd0b5cc72178e98a7356f3ec
a.chugunov <a.chugunov@kefirgames.com>
2024-08-13 13:46:27 +0300

    chg: build

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,34 +1,12 @@
-import asyncio
-import telegram
-import psutil
-import sys
+import telebot
 
+bot = telebot.TeleBot('7383514055:AAG6Kg93Lra_Unq3TmIYkzuAlPhp75J9vP0')
 
-# Get the name of the current script
-script_name = sys.argv[0]
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton("Play Game", url=f"https://likepenza1.github.io/CastleSurvivor/{message.from_user.username}")
+    markup.add(button)
+    bot.send_message(message.chat.id, "Welcome! Click the button to play the game.", reply_markup=markup)
 
-# Get a list of all running processes
-processes = psutil.process_iter(['pid', 'name'])
-
-# Check if there are any other instances of the script running
-for process in processes:
-    if process.info['name'] == script_name and process.info['pid'] != os.getpid():
-        print(f"Another instance of the script is already running with PID {process.info['pid']}. Exiting...")
-        sys.exit()
-
-# Your code to get updates from Telegram bot goes here
-
-async def main():
-    bot = telegram.Bot(token='7383514055:AAG6Kg93Lra_Unq3TmIYkzuAlPhp75J9vP0')
-    updates = await bot.get_updates()
-
-    if updates:
-        update = updates[-1]
-        username = update.message.from_user.username
-
-        with open('username.txt', 'w') as f:
-            f.write(username)
-    else:
-        print("No updates available.")
-
-asyncio.run(main())
+bot.polling()
